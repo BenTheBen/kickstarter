06586320e3020e33ae294431c362958ff4afcf5a
Scott Crawshaw <scott_crawshaw18@milton.edu>
2017-10-31 14:10:41 -0400

    I'm adding the network files, as they are completed. However, I still need to load in ben's data.

---
+++ NeuralNetworkRunner.py
@@ -0,0 +1,70 @@
+#I got this code from https://machinelearnings.co/text-classification-using-neural-networks-f5cd7b8765c6
+#I didn't change anything relevant to the structure of the network. It functions exactly as the author meant it to.
+#I played around with the error threshold.
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+import numpy as np
+import json
+stemmer = LancasterStemmer()
+def sigmoid(x):
+    output = 1/(1+np.exp(-x))
+    return output
+def clean_up_sentence(sentence):
+    # tokenize the pattern
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=False):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return(np.array(bag))
+
+def think(sentence, show_details=False):
+    x = bow(sentence.lower(), words, show_details)
+    if show_details:
+        print ("sentence:", sentence, "\n bow:", x)
+    # input layer is our bag of words
+    l0 = x
+    # matrix multiplication of input and hidden layer
+    l1 = sigmoid(np.dot(l0, synapse_0))
+    # output layer
+    l2 = sigmoid(np.dot(l1, synapse_1))
+    return l2
+# probability threshold
+ERROR_THRESHOLD = 0.2
+# load our calculated synapse values
+synapse_file = 'synapses.json' 
+with open(synapse_file) as data_file: 
+    synapse = json.load(data_file) 
+    synapse_0 = np.asarray(synapse['synapse0']) 
+    synapse_1 = np.asarray(synapse['synapse1'])
+    words = np.asarray(synapse['words'])
+    classes = np.asarray(synapse['classes'])
+
+def classify(sentence, show_details=False):
+    results = think(sentence, show_details)
+
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD ] 
+    results.sort(key=lambda x: x[1], reverse=True) 
+    return_results =[[classes[r[0]],r[1]] for r in results]
+    print ("%s \n classification: %s" % (sentence, return_results))
+    return return_results
+
+#Lets see how it works!
+classify("sudo make me a sandwich")
+classify("how are you today?")
+classify("talk to you tomorrow")
+classify("who are you?")
+classify("make me some lunch")
+classify("how was your lunch today?")
